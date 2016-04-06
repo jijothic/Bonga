9dfb0ebb8e1e91ddacd1803dca7107316015c9cf
jijothic <geogtz@yahoo.com>
2016-04-06 11:32:44 +0300

    Add first feature test

---
+++ spec/features/user_visits_homepage_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+
+RSpec.feature "User visits homepage" do
+  scenario "successully and sees a logo" do
+    visit root_path
+    except(page).to have_content "Bonga"
+  end
+end
