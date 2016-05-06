1700761914ca956823894219e9f879aba990ac2e
jijothic <geogtz@yahoo.com>
2016-05-06 16:43:47 +0300

    Add feature test

--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -1,8 +1,10 @@
 require "rails_helper"
 
-RSpec.feature "User visits homepage" do
-  scenario "successully and sees a logo" do
-    visit root_path
-    except(page).to have_content "Bonga"
+RSpec.feature "User visits homepage", :type => :feature do
+
+   scenario "successully and sees a logo" do
+
+    	visit root_path
+    	except(page).to have_content "Bonga"
   end
 end
