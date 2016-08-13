8591042481d8e1d72843aff49009b0f2d4143df0
jijothic <geogtz@yahoo.com>
2016-08-13 12:08:43 +0300

    Post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,4 @@
+# Posts Controller 
 class PostsController < ApplicationController
   before_action :authenticate_user!
 
@@ -21,6 +22,6 @@ class PostsController < ApplicationController
     end
 
     def post_params_residu
-      
+
     end
 end
