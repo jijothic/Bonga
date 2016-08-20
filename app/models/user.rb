fc77a53658fc5eb53fa08687230f8fcb9fed3b08
jijothic <geogtz@yahoo.com>
2016-08-20 18:26:11 +0300

    devise user test

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  #devise user test
+  #devise user test for sign_in
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
