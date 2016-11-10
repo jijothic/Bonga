d7da4be7b41b21345ba8d1fa97992fad22a57413
jijothic <geogtz@yahoo.com>
2016-11-10 20:01:17 +0300

    User signin feature

--- spec/support/features/user_signin_spec.rb
+++ spec/support/features/user_signin_spec.rb
@@ -1,7 +1,5 @@
-# user signin feature
 module Features
   def sign_in(user)
-    # sign_in
     visit new_user_session_path
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
