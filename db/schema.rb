6524c81c176372d87d6aaaecb2c5c9a24c4dc56b
jijothic <geogtz@yahoo.com>
2017-02-16 14:32:54 +0300

    updated database schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20160516115713) do
 
-  #These are extensions that must be enabled in order to support this database
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
